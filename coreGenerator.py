515d8c0e703a6409496c0a53463997887a15de8b
fengke <46189446+NawTun@users.noreply.github.com>
2023-08-31 08:42:20 -0500

    Update coreGenerator.py

--- coreGenerator.py
+++ coreGenerator.py
@@ -1,14 +1,9 @@
-import os
-import re
 import sys
 import shutil
-from shutil import *
 import uuid
 import time
 import json
-import subprocess
 import datetime
-import platform
 import os
 import xml.etree.ElementTree as ET
 
@@ -654,110 +649,7 @@ class Generator:
 
         return data
 
-    def testBuildFMU(self):
-        """Runs a cmake-based compilation of the generated FMU to check if the code compiles.
-        """
-
-        # generate path to /build subdir
-        buildDir = os.path.join(self.targetDirPath, "build")
-        binDir = os.path.join(self.targetDirPath, "bin/release")
-
-        self.printMsg(
-            "We are now test-building the FMU. You should first implement your FMU functionality and afterwards "
-            "build and deploy the FMU!")
-        try:
-
-            # Different script handling based on platform
-            if platform.system() == "Windows":
-
-                # call batch file to build the FMI library
-                pipe = subprocess.Popen(["build_VC_x64.bat"], shell=True, creationflags=subprocess.CREATE_NEW_CONSOLE,
-                                        cwd=buildDir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                # retrieve output and error messages
-                outputMsg, errorMsg = pipe.communicate()
-                # get return code
-                rc = pipe.returncode
-
-                # if return code is different from 0, print the error message
-                if rc != 0:
-                    self.printMsg(str(outputMsg) + "\n" + str(errorMsg))
-                    raise RuntimeError("Error during compilation of FMU.")
-
-                self.printMsg("Compiled FMU successfully")
-
-                # call batch file to build the FMI library
-                pipe = subprocess.Popen(["deploy.bat"], shell=True, creationflags=subprocess.CREATE_NEW_CONSOLE,
-                                        cwd=buildDir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                # retrieve output and error messages
-                outputMsg, errorMsg = pipe.communicate()
-                # get return code
-                rc = pipe.returncode
-
-                if rc != 0:
-                    self.printMsg(str(outputMsg) + "\n" + str(errorMsg))
-                    raise RuntimeError("Error during compilation of FMU")
-
-                self.printMsg("Successfully created {}".format(self.modelName + ".fmu"))
-
-            else:
-                # shell file execution for Mac & Linux
-                pipe = subprocess.Popen(["bash", './build.sh'], cwd=buildDir, stdout=subprocess.PIPE,
-                                        stderr=subprocess.PIPE)
-                outputMsg, errorMsg = pipe.communicate()
-                rc = pipe.returncode
-
-                if rc != 0:
-                    self.printMsg(errorMsg)
-                    raise RuntimeError("Error during compilation of FMU")
-
-                self.printMsg("Compiled FMU successfully")
-
-                # Deployment
-
-                # shell file execution for Mac & Linux
-                deploy = subprocess.Popen(["bash", './deploy.sh'], cwd=buildDir, stdout=subprocess.PIPE,
-                                          stderr=subprocess.PIPE)
-                outputMsg, errorMsg = deploy.communicate()
-                dc = deploy.returncode
-
-                if dc != 0:
-                    self.printMsg(errorMsg)
-                    raise RuntimeError("Error during assembly of FMU")
-
-                self.printMsg("Successfully created {}".format(self.modelName + ".fmu"))
-
-        except Exception as e:
-            self.printMsg(str(e))
-            self.printMsg("Error building FMU.")
-            raise
-
     def writeInputData(self, targetFile):
-        """
-        Writes
-        all
-        input
-        data
-        to
-        FMIGenerator
-        to
-        file
-        so
-        it
-        can
-        be
-        read
-        later
-        by
-        the
-        FMIGeneratorWizard
-        to
-        populate
-        the
-        dialog
-        again(greatly
-        helps in testing
-        the
-        code)"""
 
         varArray = []
 
